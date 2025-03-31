31fac8f5c9554cb4c17d1167d259f13f04fe0b48
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-31 11:27:14 +0300

    Added GET /heroes/<id> to fetch a single hero with powers

--- app.py
+++ app.py
@@ -16,3 +16,23 @@ migrate = Migrate(app, db)
 def get_heroes():
     heroes = Hero.query.all()
     return jsonify([{"id": h.id, "name": h.name, "super_name": h.super_name} for h in heroes])
+
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero(id):
+    hero = Hero.query.get(id)
+    if not hero:
+        return jsonify({"error": "Hero not found"}), 404
+    return jsonify({
+        "id": hero.id,
+        "name": hero.name,
+        "super_name": hero.super_name,
+        "hero_powers": [
+            {
+                "hero_id": hp.hero_id,
+                "id": hp.id,
+                "power": {"id": hp.power.id, "name": hp.power.name, "description": hp.power.description},
+                "power_id": hp.power_id,
+                "strength": hp.strength
+            } for hp in hero.hero_powers
+        ]
+    })
