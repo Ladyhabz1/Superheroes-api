1ec94458e5c05392a3b9380467e5ef6b19acf112
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-31 10:53:24 +0300

    Added migrations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///superheroes.db"
@@ -8,3 +9,4 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+migrate = Migrate(app, db)
