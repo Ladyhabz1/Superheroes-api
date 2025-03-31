7999ed9522d9f09433cfaa87c8a28a5c4ab08a9f
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-31 16:48:04 +0300

    recreated the database

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from extensions import db, ma
 from models import Hero, Power, HeroPower
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///superheroes.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Superheroes.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+migrate = Migrate(app, db)
 db.init_app(app)
 ma.init_app(app)
-migrate = Migrate(app, db)
+
 
 @app.route('/heroes', methods=['GET'])
 def get_heroes():
--- seed.py
+++ seed.py
@@ -1,18 +1,50 @@
-from app import app, db
-from models import Hero, Power, HeroPower
+from random import choice as rc
 
-with app.app_context():
-    db.create_all()
+from app import app
+from models import db, Hero, Power, HeroPower
 
-    hero1 = Hero(name="Kamala Khan", super_name="Ms. Marvel")
-    power1 = Power(name="flight", description="gives the wielder the ability to fly")
-    
-    db.session.add(hero1)
-    db.session.add(power1)
-    db.session.commit()
+if __name__ == '__main__':
+    with app.app_context():
+        print("Clearing db...")
+        Power.query.delete()
+        Hero.query.delete()
+        HeroPower.query.delete()
 
-    hero_power1 = HeroPower(strength="Strong", hero_id=hero1.id, power_id=power1.id)
-    db.session.add(hero_power1)
-    db.session.commit()
+        print("Seeding powers...")
+        powers = [
+            Power(name="super strength", description="gives the wielder super-human strengths"),
+            Power(name="flight", description="gives the wielder the ability to fly through the skies at supersonic speed"),
+            Power(name="super human senses", description="allows the wielder to use her senses at a super-human level"),
+            Power(name="elasticity", description="can stretch the human body to extreme lengths"),
+        ]
 
-print("Database seeded!")
+        db.session.add_all(powers)
+
+        print("Seeding heroes...")
+        heroes = [
+            Hero(name="Kamala Khan", super_name="Ms. Marvel"),
+            Hero(name="Doreen Green", super_name="Squirrel Girl"),
+            Hero(name="Gwen Stacy", super_name="Spider-Gwen"),
+            Hero(name="Janet Van Dyne", super_name="The Wasp"),
+            Hero(name="Wanda Maximoff", super_name="Scarlet Witch"),
+            Hero(name="Carol Danvers", super_name="Captain Marvel"),
+            Hero(name="Jean Grey", super_name="Dark Phoenix"),
+            Hero(name="Ororo Munroe", super_name="Storm"),
+            Hero(name="Kitty Pryde", super_name="Shadowcat"),
+            Hero(name="Elektra Natchios", super_name="Elektra"),
+        ]
+
+        db.session.add_all(heroes)
+
+        print("Adding powers to heroes...")
+        strengths = ["Strong", "Weak", "Average"]
+        hero_powers = []
+        for hero in heroes:
+            power = rc(powers)
+            hero_powers.append(
+                HeroPower(hero=hero, power=power, strength=rc(strengths))
+            )
+        db.session.add_all(hero_powers)
+        db.session.commit()
+
+        print("Done seeding!")
