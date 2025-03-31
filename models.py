c04a00fd89747137c4c50b02209d0de72135ebea
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-03-31 10:39:17 +0300

    Defined the power model with database schema

--- models.py
+++ models.py
@@ -5,3 +5,9 @@ class Hero(db.Model):
     name = db.Column(db.String, nullable=False)
     super_name = db.Column(db.String, nullable=False)
     hero_powers = db.relationship('HeroPower', backref='hero', cascade="all, delete")
+    
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    hero_powers = db.relationship('HeroPower', backref='power', cascade="all, delete")
